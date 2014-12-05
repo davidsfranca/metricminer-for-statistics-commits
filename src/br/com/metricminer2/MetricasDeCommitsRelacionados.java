24c1a944d8581ee366cba66efb5c248c41c1f5f4
davidsfranca <davidsfranca@gmail.com>
2014-12-05 14:28:49 -0200

    adicionar cabeçalho
    adicionar cabeçalho pra gerar tabela automaticamente quando tentar
    importar csv

--- src/br/com/metricminer2/MetricasDeCommitsRelacionados.java
+++ src/br/com/metricminer2/MetricasDeCommitsRelacionados.java
@@ -1,6 +1,7 @@
 package br.com.metricminer2;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import br.com.metricminer2.domain.ModificationType;
@@ -11,13 +12,25 @@ import br.com.metricminer2.scm.SourceCodeRepositoryNavigator;
 	
 public class MetricasDeCommitsRelacionados {
 	public static void main(String[] args) {
-		String repoPath = "/opt/webapps/";
-		String csvPath = "/opt/metricas.csv";
-		List<String> extesoesProcuradas = Arrays.asList("java");
-		List<String> extesoesRelacionadasProcuradas = Arrays.asList("java","xml");
+		String repoPath = "E:/webapps/";
+		String csvPath = "E:/metricas.csv";
+		List<String> extensoesProcuradas = Arrays.asList("xml", "java");
+		List<String> extensoesRelacionadasProcuradas = Arrays.asList("java");
 		List<ModificationType> tipoDeCommit = Arrays.asList(ModificationType.ADD, ModificationType.MODIFY);
+		CSVFile writer = new CSVFile(csvPath);
+		gerarCabecalhoCSV(writer, extensoesRelacionadasProcuradas);
 		new SourceCodeRepositoryNavigator(new MMOptions())
-		.in(GitRepository.allIn(repoPath)).process(new ContadorCommitsEArquivosRelacionados(extesoesProcuradas,extesoesRelacionadasProcuradas,tipoDeCommit),
-				new CSVFile(csvPath)).start();;
+		.in(GitRepository.allIn(repoPath)).process(new ContadorCommitsEArquivosRelacionados(extensoesProcuradas,extensoesRelacionadasProcuradas,tipoDeCommit),writer).start();
+	}
+
+	private static void gerarCabecalhoCSV(CSVFile writer, List<String> extensoesRelacionadasProcuradas) {
+		String cabecalho = "id,nome_arquivo,commiter,tipo_commit,data";
+		 Iterator<String> i = extensoesRelacionadasProcuradas.iterator();
+         while (i.hasNext()){
+        	 	cabecalho+=",";
+                String it = i.next();
+                cabecalho+=it+",qnt_"+it;
+         }
+         writer.write(cabecalho);
 	}
 }
